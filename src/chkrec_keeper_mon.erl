55f4e15a7bcc3213de2df5bd457045baf57b7ea4
Max Hirschhorn <maxh@caltech.edu>
2014-05-25 15:27:16 -0700

    Added `chkrec_keeper_mon:watch/2` function.
    Calls `erlang:monitor/2` to keep track of when a `chkrec_keeper`
    process goes down. This is done so that subsequent calls to
    `chkrec_keeper_mon:lookup/1` will return the new pid. However, the
    calls to `chkrec_keeper_mon:watch/2` and `chkrec_keeper_mon:update/2`
    still need to be integrated into the initialization phase of the
    backup keeper (i.e. `chkrec_keeper:init/1`) so the new pid is
    actually stored when the process is restarted.
    
    Note that the entry to the backup keeper is never removed, but can
    only be overwritten. A better approach may be to only change the state
    to `down` if the process had exited abnormally, and remove the entry
    otherwise. (We can do this since we are using a `transient` restart
    strategy for the `chkrec_keeper`s.)

--- src/chkrec_keeper_mon.erl
+++ src/chkrec_keeper_mon.erl
@@ -8,7 +8,7 @@
 -define(TIMEOUT, 5000).
 -define(KEEPER_SUP, chkrec_keeper_sup).
 
--export([new/1, update/2, lookup/1, start_link/0]).
+-export([new/1, update/2, lookup/1, watch/2, start_link/0]).
 
 -export([
     init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2
@@ -17,10 +17,26 @@
 
 -export_type([maybe_pid/0]).
 
--type maybe_pid() :: chkrec_keeper:maybe(pid()).
+-type maybe_pid() :: chkrec_keeper:maybe(pid()) | down.
+%% Optionally present pid.
 
 -type registry() :: dict:dict(term(), maybe_pid()).
 %% Mapping of identifiers for useful workers to pids of backup keepers.
+
+-type refs() :: dict:dict(reference(), term()).
+%% Mapping of references for monitored backup keepers to identifiers for
+%% useful workers.
+
+-record(keeper_mon, {
+    registry = dict:new() :: registry()
+  , refs = dict:new() :: refs()
+}).
+
+-type keeper_mon() :: #keeper_mon{
+    registry :: registry()
+  , refs :: refs()
+}.
+%% Internal backup keeper monitor state.
 
 %%====================================================================
 %% API
@@ -59,6 +75,13 @@ lookup(Source) ->
     gen_server:call(?MODULE, {lookup, Source}, ?TIMEOUT)
 .
 
+-spec watch(Source :: term(), Keeper :: pid()) -> ok.
+
+%% @doc TODO document
+watch(Source, Keeper) when is_pid(Keeper) ->
+    gen_server:cast(?MODULE, {watch, Source, Keeper})
+.
+
 %%--------------------------------------------------------------------
 
 -spec start_link() ->
@@ -72,26 +95,27 @@ start_link() -> gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 %%--------------------------------------------------------------------
 
 -spec init(Args :: list()) ->
-    {ok, State :: registry()}
+    {ok, State :: keeper_mon()}
   | {stop, Reason :: term()}
 .
 
 %% @doc Initializes the internal state of the backup keeper monitor.
-init([]) -> {ok, dict:new()}.
+init([]) -> {ok, #keeper_mon{}}.
 
 %%--------------------------------------------------------------------
 
 -spec handle_call(
     Request :: term()
   , From :: {pid(), Tag :: term()}
-  , State0 :: registry()
+  , State0 :: keeper_mon()
 ) ->
-    {reply, Reply :: term(), State1 :: registry()}
+    {reply, Reply :: term(), State1 :: keeper_mon()}
 .
 
 %% @doc TODO document
 handle_call({lookup, Source}, _From, State) ->
-    MaybeKeeper = case dict:find(Source, State) of
+    Registry = State#keeper_mon.registry
+  , MaybeKeeper = case dict:find(Source, Registry) of
         {ok, Option} -> Option
       ; error -> none
     end
@@ -107,14 +131,24 @@ handle_call(_Request, _From, State) ->
 
 -spec handle_cast(
     Request :: term()
-  , State0 :: registry()
+  , State0 :: keeper_mon()
 ) ->
-    {noreply, State1 :: registry()}
+    {noreply, State1 :: keeper_mon()}
 .
 
 %% @doc TODO document
 handle_cast({update, Source, Keeper}, State0) ->
-    State1 = dict:store(Source, Keeper, State0)
+    Registry0 = State0#keeper_mon.registry
+  , Registry1 = dict:store(Source, Keeper, Registry0)
+  , State1 = State0#keeper_mon{registry=Registry1}
+  , {noreply, State1}
+;
+
+handle_cast({watch, Source, Keeper}, State0) ->
+    Refs0 = State0#keeper_mon.refs
+  , MonitorRef = erlang:monitor(process, Keeper)
+  , Refs1 = dict:store(MonitorRef, Source, Refs0)
+  , State1 = State0#keeper_mon{refs=Refs1}
   , {noreply, State1}
 ;
 
@@ -126,19 +160,35 @@ handle_cast(_Request, State) ->
 
 -spec handle_info(
     Info :: timeout | term()
-  , State0 :: registry()
+  , State0 :: keeper_mon()
 ) ->
-    {noreply, State1 :: registry()}
+    {noreply, State1 :: keeper_mon()}
 .
 
 %% @doc TODO document
+handle_info(
+    {'DOWN', MonitorRef, process, _Keeper, _Reason}
+  , State0 = #keeper_mon{registry = Registry0, refs = Refs0}
+) ->
+    % Assumed that `MonitorRef' is present in the mapping of references.
+    Source = dict:fetch(MonitorRef, Refs0)
+  , Refs1 = dict:erase(MonitorRef, Refs0)
+
+    % Assumed that `Source' is present in the registry, and is
+    % associated with `{value, _Keeper}'.
+  , Registry1 = dict:store(Source, down, Registry0)
+
+  , State1 = State0#keeper_mon{registry=Registry1, refs=Refs1}
+  , {noreply, State1}
+;
+
 handle_info(_Info, State) ->
     {noreply, State}
 .
 
 %%--------------------------------------------------------------------
 
--spec terminate(Reason :: term(), State :: registry()) -> ok.
+-spec terminate(Reason :: term(), State :: keeper_mon()) -> ok.
 
 %% @doc Called by a `gen_server' when it is about to terminate.
 %%      Nothing to clean up though.
@@ -146,10 +196,10 @@ terminate(_Reason, _State) -> ok.
 
 -spec code_change(
     OldVsn :: term()
-  , State0 :: registry()
+  , State0 :: keeper_mon()
   , Extra :: term()
 ) ->
-    {ok, State1 :: registry()}
+    {ok, State1 :: keeper_mon()}
 .
 
 %% @doc Called by a `gen_server' when it should update its internal
